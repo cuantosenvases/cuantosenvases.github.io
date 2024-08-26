4b4abf4c9d706f8660dc7e21c29e4552f945a86a
German Mazza <gemazza@unlam.edu.ar>
2024-08-26 17:11:11 -0300

    First commit

---
+++ assets/js/packaging.js
@@ -0,0 +1,21 @@
+function howManyContainers(){
+    let liters = document.getElementById("liters");
+    let packaging = document.getElementById("packaging");
+    let quantity = null;
+    let message = "";
+
+    if (liters.value != "") {
+        if (packaging.value == 7){
+            quantity = liters.value / 0.007;
+        } else {
+            quantity = liters.value / 0.05;
+        }
+
+        message = "Necesitas " + Math.ceil(quantity) + " envases de " + packaging.options[packaging.selectedIndex].text;
+    } else {
+        message = "Es necesario indicar los litros!";
+        liters.focus();
+    }
+
+    document.getElementById("quantity").innerHTML = message;
+}
